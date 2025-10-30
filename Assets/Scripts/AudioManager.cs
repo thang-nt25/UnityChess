bac7c02c579d8396db2e70f03530bd3460cc2f52
Tho0205 <tho02052004@gmail.com>
2025-10-30 15:09:18 +0700

    Update Audio mixer for Music launcher and SFX Volume
    - Ad Game Audio Mixer for Game
    + 3 Slider Music : 1 For Master Music, 1 For SFX music and 1 For Chess'move

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+    public Slider musicSlider;
+    public Slider sfxSlider;
+
+    private const float minVolume = 0.0001f; 
+    private const float muteVolume = -80f;  
+
+    void Start()
+    {
+        PlayerPrefs.DeleteKey("MusicVolume");
+        PlayerPrefs.DeleteKey("SFXVolume");
+
+        float musicVol = PlayerPrefs.GetFloat("MusicVolume", 1f);
+        float sfxVol = PlayerPrefs.GetFloat("SFXVolume", 1f);
+
+        musicSlider.value = musicVol;
+        sfxSlider.value = sfxVol;
+
+        ApplyMusicVolume(musicVol);
+        ApplySFXVolume(sfxVol);
+
+        musicSlider.onValueChanged.AddListener(SetMusicVolume);
+        sfxSlider.onValueChanged.AddListener(SetSFXVolume);
+    }
+
+    public void SetMusicVolume(float volume)
+    {
+        ApplyMusicVolume(volume);
+        PlayerPrefs.SetFloat("MusicVolume", volume);
+    }
+
+    public void SetSFXVolume(float volume)
+    {
+        ApplySFXVolume(volume);
+        PlayerPrefs.SetFloat("SFXVolume", volume);
+    }
+
+    private void ApplyMusicVolume(float volume)
+    {
+        float dB = (volume <= 0.0001f) ? muteVolume : Mathf.Log10(volume) * 20;
+        audioMixer.SetFloat("MusicVolume", dB);
+    }
+
+    private void ApplySFXVolume(float volume)
+    {
+        float dB = (volume <= 0.0001f) ? muteVolume : Mathf.Log10(volume) * 20;
+        audioMixer.SetFloat("SFXVolume", dB);
+    }
+}
